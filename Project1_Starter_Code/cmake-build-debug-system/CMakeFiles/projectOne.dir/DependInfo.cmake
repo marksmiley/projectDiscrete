fabad772b900e11cd29299951347d7b8b5f0d272
marcus <marcus81smiley@gmail.com>
2021-12-02 15:25:29 -0700

    fixed a small typo in CombineTuples after a good lunch.

---
+++ Project1_Starter_Code/cmake-build-debug-system/CMakeFiles/projectOne.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/AddAutomaton.cpp" "CMakeFiles/projectOne.dir/AddAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/AddAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/ColonAutomaton.cpp" "CMakeFiles/projectOne.dir/ColonAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/ColonAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/ColonDashAutomaton.cpp" "CMakeFiles/projectOne.dir/ColonDashAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/ColonDashAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/CommaAutomaton.cpp" "CMakeFiles/projectOne.dir/CommaAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/CommaAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/CommentAutomata.cpp" "CMakeFiles/projectOne.dir/CommentAutomata.cpp.o" "gcc" "CMakeFiles/projectOne.dir/CommentAutomata.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Database.cpp" "CMakeFiles/projectOne.dir/Database.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Database.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/DatalogProgram.cpp" "CMakeFiles/projectOne.dir/DatalogProgram.cpp.o" "gcc" "CMakeFiles/projectOne.dir/DatalogProgram.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/FactsAutomaton.cpp" "CMakeFiles/projectOne.dir/FactsAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/FactsAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Header.cpp" "CMakeFiles/projectOne.dir/Header.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Header.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/IDAutomaton.cpp" "CMakeFiles/projectOne.dir/IDAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/IDAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Interpreter.cpp" "CMakeFiles/projectOne.dir/Interpreter.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Interpreter.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Left_ParenAutomaton.cpp" "CMakeFiles/projectOne.dir/Left_ParenAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Left_ParenAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Lexer.cpp" "CMakeFiles/projectOne.dir/Lexer.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Lexer.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/MultiplyAutomaton.cpp" "CMakeFiles/projectOne.dir/MultiplyAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/MultiplyAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Parameter.cpp" "CMakeFiles/projectOne.dir/Parameter.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Parameter.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/PeriodAutomaton.cpp" "CMakeFiles/projectOne.dir/PeriodAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/PeriodAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Predicate.cpp" "CMakeFiles/projectOne.dir/Predicate.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Predicate.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Q_MarkAutomaton.cpp" "CMakeFiles/projectOne.dir/Q_MarkAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Q_MarkAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/QueriesAutomaton.cpp" "CMakeFiles/projectOne.dir/QueriesAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/QueriesAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Relation.cpp" "CMakeFiles/projectOne.dir/Relation.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Relation.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Right_ParenAutomaton.cpp" "CMakeFiles/projectOne.dir/Right_ParenAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Right_ParenAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Rule.cpp" "CMakeFiles/projectOne.dir/Rule.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Rule.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/RulesAutomaton.cpp" "CMakeFiles/projectOne.dir/RulesAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/RulesAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/SchemesAutomaton.cpp" "CMakeFiles/projectOne.dir/SchemesAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/SchemesAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/StringAutomaton.cpp" "CMakeFiles/projectOne.dir/StringAutomaton.cpp.o" "gcc" "CMakeFiles/projectOne.dir/StringAutomaton.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Token.cpp" "CMakeFiles/projectOne.dir/Token.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Token.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/Tuple.cpp" "CMakeFiles/projectOne.dir/Tuple.cpp.o" "gcc" "CMakeFiles/projectOne.dir/Tuple.cpp.o.d"
+  "/Users/marcus/Desktop/School/discretestOfStructures/projectDiscrete/Project1_Starter_Code/main.cpp" "CMakeFiles/projectOne.dir/main.cpp.o" "gcc" "CMakeFiles/projectOne.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
